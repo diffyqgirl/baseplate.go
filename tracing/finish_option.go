899f488362e413556ace5c49053b6b8d2e746efa
Laura Ungar <laura.ungar@reddit.com>
2022-11-04 15:52:32 -0700

    logging in Convert

--- tracing/finish_option.go
+++ tracing/finish_option.go
@@ -2,9 +2,11 @@ package tracing
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
+	"github.com/reddit/baseplate.go/log"
 )
 
 const (
@@ -28,14 +30,19 @@ type FinishOptions struct {
 func (fo FinishOptions) Convert() opentracing.FinishOptions {
 	var opts opentracing.FinishOptions
 	var lr opentracing.LogRecord
-	if fo.Ctx != nil {
+	if fo.Ctx != nil {	
+		log.Warnw("in Convert ctx, fo.Ctx is: " + fmt.Sprintf("%+v\n", fo.Ctx))
+		log.Warnw("in Convert ctx, appending: " + fmt.Sprintf("%+v\n", log.Object(ctxKey, fo.Ctx)))
 		lr.Fields = append(lr.Fields, log.Object(ctxKey, fo.Ctx))
 	}
 	if fo.Err != nil {
+		log.Warnw("in Convert err, fo.Err is: " + fmt.Sprintf("%+v\n", fo.Err))
+		log.Warnw("in Convert err, appending: " + fmt.Sprintf("%+v\n", log.Error(fo.Err)))
 		lr.Fields = append(lr.Fields, log.Error(fo.Err))
 	}
 	if len(lr.Fields) > 0 {
 		opts.LogRecords = append(opts.LogRecords, lr)
+		log.Warnw("in Convert, log records is " + fmt.Sprintf("%+v\n", lr))
 	}
 	return opts
 }
